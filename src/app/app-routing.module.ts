4dc4ab5a23931167b56f98767a014f81d13d240a
BorisConcha <borisconchasagardia@gmail.com>
2025-07-14 17:54:31 -0400

    arreglo enrutamiento de las paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerfumeListComponent } from './components/perfume-list/perfume-list.component';
 import { PerfumeDetailComponent } from './components/perfume-detail/perfume-detail.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { LoginComponent } from './components/auth/login/login/login.component';
+import { RegisterComponent } from './components/auth/register/register/register.component';
+import { ForgotPasswordComponent } from './components/auth/forgotPassword/forgot-password/forgot-password.component';
+import { ProfileComponent } from './components/profile/profile/profile.component';
+import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AdminGuard } from './guards/admin.guard';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/perfumes', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'perfumes', component: PerfumeListComponent },
   { path: 'perfumes/:id', component: PerfumeDetailComponent },
-  { path: '**', redirectTo: '/perfumes' }
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'admin', component: AdminDashboardComponent, canActivate: [AdminGuard] },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
